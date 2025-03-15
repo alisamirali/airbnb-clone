ecc6440d431c07c9217d2251245ac616fe61c85b
Ali Samir <ali.samir@taager.com>
2025-03-15 16:24:51 +0200

    resolve buid api issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+export const dynamic = "force-dynamic";
+
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListings from "@/app/actions/getListings";
 import { ClientOnly, Container, EmptyState } from "@/app/components";
 import ListingCard from "@/app/components/listings/ListingCard";
 
-const Home = async ({ searchParams }: any) => {
+const Home = async ({
+  searchParams,
+}: {
+  searchParams: { userId?: string };
+}) => {
   const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
@@ -20,7 +25,7 @@ const Home = async ({ searchParams }: any) => {
     <ClientOnly>
       <Container>
         <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-          {listings.map((listing: any) => (
+          {listings.map((listing) => (
             <ListingCard
               currentUser={currentUser}
               key={listing.id}
