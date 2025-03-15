b48d6ff5229804dc6342c8abde35aa09a291c56e
Ali Samir <ali.samir@taager.com>
2025-03-15 16:05:23 +0200

    resolve buid api issues

--- app/api/reservations/route.ts
+++ app/api/reservations/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
 import getCurrentUser from "@/app/actions/getCurrentUser";
@@ -6,31 +7,33 @@ export async function POST(request: Request) {
   const currentUser = await getCurrentUser();
 
   if (!currentUser) {
-    return NextResponse.error();
+    return new NextResponse("Unauthorized", { status: 401 });
   }
 
   const body = await request.json();
   const { listingId, startDate, endDate, totalPrice } = body;
 
   if (!listingId || !startDate || !endDate || !totalPrice) {
-    return NextResponse.error();
+    return new NextResponse("Bad Request", { status: 400 });
   }
 
-  const listingAndReservation = await prisma.listing.update({
-    where: {
-      id: listingId,
-    },
-    data: {
-      reservations: {
-        create: {
-          userId: currentUser.id,
-          startDate,
-          endDate,
-          totalPrice,
+  try {
+    const listingAndReservation = await prisma.listing.update({
+      where: { id: listingId },
+      data: {
+        reservations: {
+          create: {
+            userId: currentUser.id,
+            startDate,
+            endDate,
+            totalPrice,
+          },
         },
       },
-    },
-  });
+    });
 
-  return NextResponse.json(listingAndReservation);
+    return NextResponse.json(listingAndReservation);
+  } catch (error) {
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
